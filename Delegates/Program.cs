40957831922fd7085b7f47cff3daa4498604c09e
Filipe Silva <filipeqs@outlook.com>
2021-07-23 08:09:59 -0400

    delegate

---
+++ Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var photoProcessor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += ApplyBackground;
+
+            photoProcessor.Process("", filterHandler);
+        }
+
+        static void ApplyBackground(Photo photo)
+        {
+            Console.WriteLine("Apply Background");
+        }
+    }
+}
