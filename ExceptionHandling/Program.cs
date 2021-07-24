6c7d2f8129942a65193075540f9a68daf4612696
Filipe Silva <filipeqs@outlook.com>
2021-07-24 07:49:18 -0400

    exception handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CustomException();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sorry, an error ocurred");
+            }
+        }
+
+        static void CustomException()
+        {
+            var api = new YouTubeApi();
+            var videos = api.GetVideos("filipe");
+        }
+
+        static void CalculatorError()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Divide(5, 0);
+        }
+
+        static void StreamReaderError()
+        {
+            using (var streamReader = new StreamReader(@"c:\file.zip"))
+            {
+                var content = streamReader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/YouTubeApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandling
+{
+
+    public class YouTubeApi
+    {
+        public List<Video> GetVideos(string user)
+        {
+            try
+            {
+                // Access YouTube Web Service
+                // Read the data
+                // Create a list of videos
+                throw new Exception("Oops sme low level youtube error");
+                return new List<Video>();
+            }
+            catch (Exception ex)
+            {
+                // Log
+
+                throw new YouTubeException("Could not fetch de videos from YouTube", ex);
+            }
+        }
+    }
+}
