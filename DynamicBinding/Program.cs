b9fca41f09d6862f7a127cd89b72a759a4b09199
Filipe Silva <filipeqs@outlook.com>
2021-07-23 21:32:02 -0400

    dynamic

---
+++ DynamicBinding/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DynamicBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic name = "Filipe";
+            name = 10;
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b;
+
+            int i = 5;
+            dynamic d = i;
+            long l = d;
+        }
+    }
+}
